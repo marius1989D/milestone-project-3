d587bad603402dc6d4ef358b808e1b167e1a119b
Ubuntu <ubuntu@ip-172-31-92-62.ec2.internal>
2019-07-28 20:05:13 +0000

    added templates folder and recipes.html

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'cooking_recipes'
+app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@myfirstcluster-rffnv.mongodb.net/cooking_recipes?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+
 @app.route('/')
-def hello():
-    return 'Hello...'
+@app.route('/get_recipe')
+def get_recipe():
+    return render_template('recipes.html', recipes=mongo.db.recipes.find())
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
