9e0613262e8decd38d41b09400bcbcffdda33467
Ubuntu <ubuntu@ip-172-31-92-62.ec2.internal>
2019-07-31 18:34:19 +0000

    added 'delete route'

--- app.py
+++ app.py
@@ -45,6 +45,11 @@ def update_recipe(recipe_id):
     })
     return redirect(url_for('get_recipe'))
     
+@app.route('/delete_recipe/<recipe_id>')
+def delete_recipe(recipe_id):
+    mongo.db.recipes.remove({'_id': ObjectId(recipe_id)})
+    return redirect(url_for('get_recipe'))
+    
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
         port=int(os.environ.get('PORT')),
