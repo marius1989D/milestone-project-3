7a023a27141fe2b1217adecc507486aa3d98699d
Ubuntu <ubuntu@ip-172-31-92-62.ec2.internal>
2019-07-31 18:14:03 +0000

    added edit function

--- app.py
+++ app.py
@@ -26,6 +26,12 @@ def insert_recipe():
     recipes.insert_one(request.form.to_dict())
     return redirect(url_for('get_recipe'))
     
+@app.route('/edit_recipe/<recipe_id>', methods=['POST', 'GET'])
+def edit_recipe(recipe_id):
+    the_recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    all_categories =  mongo.db.categories.find()
+    return render_template('editrecipe.html', recipe=the_recipe, categories=all_categories)
+    
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
         port=int(os.environ.get('PORT')),
